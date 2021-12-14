94372b88551847360b5f838133afec78956951f8
Ismail Moukafih <i.moukafih@dbf.nl>
2021-12-14 09:56:40 +0100

    php 8.0

--- src/MoukailPasswordResetMailBundle.php
+++ src/MoukailPasswordResetMailBundle.php
@@ -2,12 +2,14 @@
 
 namespace Moukail\PasswordResetMailBundle;
 
-use Moukail\PasswordResetMailBundle\DependencyInjection\MoukailPasswordResetMailExtention;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+use Moukail\PasswordResetMailBundle\DependencyInjection\MoukailPasswordResetMailExtention;
 
 class MoukailPasswordResetMailBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new MoukailPasswordResetMailExtention();
